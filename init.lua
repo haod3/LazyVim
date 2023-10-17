f849140d2cfbf299026907e51e21ec6183922248
haod3 <holden_s@163.com>
2023-10-17 21:49:23 +0800

    feat: support vscode

--- init.lua
+++ init.lua
@@ -1,2 +1,17 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+
+if vim.g.vscode then
+  -- VSCode extention
+  local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+  vim.opt.rtp:prepend(lazypath)
+  require("lazy").setup({
+    spec = {
+      { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+      { import = "lazyvim.plugins.extras.vscode" },
+      { import = "plugins" },
+    },
+  })
+else
+  -- ordinary Neovim
+  require("config.lazy")
+end
